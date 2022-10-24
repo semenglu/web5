77e10618a5a78d98d8440ecd9d9a570655f7a087
semenglu <semengluhovsky@mail.ru>
2022-10-24 22:08:35 +0300

    init

---
+++ kalk.js
@@ -0,0 +1,20 @@
+
+function proiz() 
+{
+      const check = /^\d*\.?\d+$/;
+    let a1 = document.getElementsByName("num");
+      if (check.test(a1[0].value) == false || a1[0].value == 0) 
+      {
+        alert("Вводите внимательней!");    
+    }
+      else
+      {
+        a1[0]=Number.parseInt(a1[0]);
+        a3.innerHTML="Ответ: "+c;
+      }
+}
+window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("Mbutton");
+    b.addEventListener("click", proiz);
+  });
